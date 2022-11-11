1b6a3b681860808f53e0154a44c48a31eefe94c8
ahmadhamad55 <115878996+ahmadhamad55@users.noreply.github.com>
2022-11-11 16:03:18 +0100

    create  conf

---
+++ build/html/_sources/index.md.txt
@@ -0,0 +1,10 @@
+# Ahmad
+Bienvenue sur mon portfolio
+
+```{toctree}
+:maxdepth: 2
+:hidden:
+about
+project
+blog
+```
--- source/conf.py
+++ source/conf.py
@@ -13,15 +13,31 @@ author = 'Ahmad Hamad'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ["myst_parser"]
 
 templates_path = ['_templates']
 exclude_patterns = []
 
-
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.txt': 'markdown',
+    '.md': 'markdown',
+}
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'pydata_sphinx_theme'
+html_theme_options = {
+  "github_url": "https://github.com/ahmadhamad55",
+  "icon_links": [
+        {
+            "name": "LinkedIn",
+            "url": "https:www.linkedin.com/in/ahmad-hamad-b60a67213",
+            "icon": "fa-brands fa-linkedin",
+        },        
+        ],
+  "search_bar_text": "Search this site...",
+}
+
 html_static_path = ['_static']
